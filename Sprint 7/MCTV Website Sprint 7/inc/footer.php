a812d97dd5895919e678c97e4ec66789889ae918
SABGroupB <x16465134@student.ncirl.ie>
2017-04-09 20:28:07 +0100

    test

--- Sprint 7/MCTV Website Sprint 7/inc/footer.php
+++
@@ -1,7 +0,0 @@
-  <footer class="site-footer black">
-    <div class="container">
-      <p>&copy; MCTV 2017</p>
-    </div>
-  </footer>
-  </body>
-</html>
--- Sprint 7/MCTV Website Sprint 7/public_html/dashboard/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include_once $_SERVER["DOCUMENT_ROOT"] . '/../inc/dashboard-header.php';
